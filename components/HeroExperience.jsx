c26672bcccadbf8d0856c116ba1eb65d754fb89c
Chase Ellsworth <chaseellsworthiv@gmail.com>
2026-03-14 11:20:52 -0400

    Tighten curtain seam under swells

--- components/HeroExperience.jsx
+++ components/HeroExperience.jsx
@@ -351,10 +351,10 @@ export default function HeroExperience() {
         <svg className={styles.curtainClipDefs} aria-hidden="true" width="0" height="0" focusable="false">
           <defs>
             <clipPath id="curtain-body-handoff" clipPathUnits="objectBoundingBox">
-              <path d="M 0 0.205 Q 0.08 0.195, 0.16 0.282 Q 0.24 0.33, 0.34 0.225 Q 0.42 0.165, 0.5 0.31 Q 0.58 0.165, 0.66 0.225 Q 0.76 0.33, 0.84 0.282 Q 0.92 0.195, 1 0.205 L 1 1 L 0 1 Z" />
+              <path d="M 0 0.17 C 0.07 0.225, 0.15 0.235, 0.22 0.188 C 0.28 0.152, 0.36 0.152, 0.43 0.205 C 0.46 0.228, 0.49 0.238, 0.5 0.238 C 0.51 0.238, 0.54 0.228, 0.57 0.205 C 0.64 0.152, 0.72 0.152, 0.78 0.188 C 0.85 0.235, 0.93 0.225, 1 0.17 L 1 1 L 0 1 Z" />
             </clipPath>
             <clipPath id="curtain-swells-cap" clipPathUnits="objectBoundingBox">
-              <path d="M 0 0 L 1 0 L 1 0.205 Q 0.92 0.195, 0.84 0.282 Q 0.76 0.33, 0.66 0.225 Q 0.58 0.165, 0.5 0.31 Q 0.42 0.165, 0.34 0.225 Q 0.24 0.33, 0.16 0.282 Q 0.08 0.195, 0 0.205 Z" />
+              <path d="M 0 0 L 1 0 L 1 0.17 C 0.93 0.225, 0.85 0.235, 0.78 0.188 C 0.72 0.152, 0.64 0.152, 0.57 0.205 C 0.54 0.228, 0.51 0.238, 0.5 0.238 C 0.49 0.238, 0.46 0.228, 0.43 0.205 C 0.36 0.152, 0.28 0.152, 0.22 0.188 C 0.15 0.235, 0.07 0.225, 0 0.17 Z" />
             </clipPath>
             <clipPath id="curtain-left-clip" clipPathUnits="objectBoundingBox">
               <path ref={leftClipPathRef} d="M 0 0 L 0.5 0 L 0.5 0.245 C 0.5 0.305, 0.5 0.395, 0.5 0.52 C 0.5 0.64, 0.5 0.79, 0.5 1 L 0 1 Z" />
